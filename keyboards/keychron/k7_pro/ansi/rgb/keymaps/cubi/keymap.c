2730548501d6c740a7278b1f6647c2d74217f65c
cubimon <cubimon93@gmail.com>
2023-11-28 19:40:17 +0100

    change some modifier keys

--- keyboards/keychron/k7_pro/ansi/rgb/keymaps/cubi/keymap.c
+++ keyboards/keychron/k7_pro/ansi/rgb/keymaps/cubi/keymap.c
@@ -152,11 +152,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,        KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [CUBI_BASE] = LAYOUT_ansi_68(
-     QK_GRAVE_ESCAPE,        KC_1,     KC_2,   KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,        KC_MINS,                 KC_EQL,  KC_BSPC, KC_DEL,
-     LT(CUBI_MOUSE, KC_TAB), KC_Q,     KC_W,   KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,        KC_LBRC,                 KC_RBRC, KC_BSLS, KC_HOME,
-     MO(CUBI_SYMBOL),        KC_A,     KC_S,   KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,     LT(CUBI_ARROW, KC_QUOT),          KC_ENT,  KC_PGUP,
-     LSFT_T(KC_ESC),         KC_Z,     KC_X,   KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                              KC_RSFT, KC_UP,   KC_PGDN,
-     KC_LCTL, KC_LOPTN,      KC_LCMMD,                             KC_SPC,                                 KC_RCMMD, MO(MAC_FN1), MO(FN2),                 KC_LEFT, KC_DOWN, KC_RGHT),
+     QK_GRAVE_ESCAPE,        KC_1,     KC_2,   KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,            KC_0,            KC_MINS,                  KC_EQL,  KC_BSPC, KC_DEL,
+     LT(CUBI_MOUSE, KC_TAB), KC_Q,     KC_W,   KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,            KC_P,            KC_LBRC,                  KC_RBRC, KC_BSLS, KC_HOME,
+     MO(CUBI_SYMBOL),        KC_A,     KC_S,   KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,            KC_SCLN,         LT(CUBI_ARROW, KC_QUOT),           KC_ENT,  KC_PGUP,
+     LSFT_T(KC_ESC),         KC_Z,     KC_X,   KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  LSFT_T(KC_DOT),  LCTL_T(KC_SLSH),                           KC_RSFT, KC_UP,   KC_PGDN,
+     KC_LCTL, KC_LOPTN,      KC_LCMMD,                             KC_SPC,                                 KC_RCMMD,        MO(MAC_FN1),     MO(FN2),                  KC_LEFT, KC_DOWN, KC_RGHT),
 
 // [CUBI_BASE] = LAYOUT_ansi_68(
 //      QK_GRAVE_ESCAPE,        KC_1,            KC_2,           KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,             KC_0,            KC_MINS,                 KC_EQL,  KC_BSPC,           KC_DEL,
@@ -169,8 +169,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_BSPC,  KC_UP,    KC_DEL,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END,    KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_LCTL,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                 KC_LSFT,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                 KC_LCTL,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [CUBI_MOUSE] = LAYOUT_ansi_68(
      KC_TRNS,  KC_TRNS,  KC_TRNS,              KC_MOUSE_WHEEL_UP,    KC_TRNS,               KC_TRNS,  KC_TRNS,               KC_TRNS,          KC_MOUSE_UP,      KC_TRNS,          KC_TRNS,                 KC_TRNS,                KC_TRNS,  KC_TRNS,            KC_TRNS,
@@ -182,16 +182,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [CUBI_SYMBOL] = LAYOUT_ansi_68(
      KC_TRNS,  KC_TRNS,           KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,           KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  MO(CUBI_FUNCTION), KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,  MO(CUBI_FUNCTION), KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_0,  KC_TRNS,            KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,           KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,           KC_TRNS,                               KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+     KC_TRNS,  KC_TRNS,           KC_TRNS,                               KC_LCTL,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [CUBI_FUNCTION] = LAYOUT_ansi_68(
      KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_F19,  KC_F20,  KC_F21,  KC_F24,  KC_TRNS,  KC_F7,    KC_F8,   KC_F9,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_F16,  KC_F17,  KC_F18,  KC_F23,  KC_TRNS,  KC_F4,    KC_F5,   KC_F6,   KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_F13,  KC_F14,  KC_F15,  KC_F22,  KC_TRNS,  KC_F1,    KC_F2,   KC_F3,   KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                            KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+     KC_TRNS,  KC_TRNS,  KC_F19,  KC_F20,  KC_F21,  KC_F24,  KC_TRNS,  KC_F7,    KC_F8,   KC_F9,   KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_F16,  KC_F17,  KC_F18,  KC_F23,  KC_TRNS,  KC_F4,    KC_F5,   KC_F6,   KC_F11,  KC_TRNS,          KC_TRNS,          KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_F13,  KC_F14,  KC_F15,  KC_F22,  KC_TRNS,  KC_F1,    KC_F2,   KC_F3,   KC_F10,                    KC_TRNS, KC_TRNS, KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                            KC_LCTL,                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
