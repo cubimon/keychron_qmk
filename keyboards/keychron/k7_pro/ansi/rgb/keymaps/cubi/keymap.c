0cd17fb13d45421541821559583f85aec0ee188c
cubimon <cubimon93@gmail.com>
2024-08-16 19:19:58 +0200

    different layer, fix esc, macro

--- keyboards/keychron/k7_pro/ansi/rgb/keymaps/cubi/keymap.c
+++ keyboards/keychron/k7_pro/ansi/rgb/keymaps/cubi/keymap.c
@@ -117,18 +117,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL,            KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(MAC_FN1),MO(FN2), KC_LEFT,  KC_DOWN, KC_RGHT),
 
 [WIN_BASE] = LAYOUT_ansi_68(
-     LT(META, QK_GRAVE_ESCAPE),  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,                     KC_MINS,                 KC_EQL,  KC_BSPC,          KC_DEL,
-     LT(CUBI_MOUSE, KC_TAB),     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,                     KC_LBRC,                 KC_RBRC, KC_BSLS,          KC_HOME,
-     MO(CUBI_SYMBOL),            KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     LT(CUBI_ARROW, KC_SCLN),  LT(CUBI_ARROW, KC_QUOT),          KC_ENT,           KC_PGUP,
-     LSFT_T(KC_ESC),             KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                                                    KC_RSFT, KC_UP,   KC_PGDN,
-     KC_LCTL,                    KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,                  MO(MAC_FN1),             MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT),
+     QK_GRAVE_ESCAPE,        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,                     KC_MINS,                 KC_EQL,  KC_BSPC,          KC_DEL,
+     LT(CUBI_MOUSE, KC_TAB), KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,                     KC_LBRC,                 KC_RBRC, KC_BSLS,          KC_HOME,
+     MO(CUBI_SYMBOL),        KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     LT(CUBI_ARROW, KC_SCLN),  LT(CUBI_ARROW, KC_QUOT),          KC_ENT,           KC_PGUP,
+     LSFT_T(KC_ESC),         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                                                    KC_RSFT, KC_UP,   KC_PGDN,
+     KC_LCTL,                KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,                  MO(MAC_FN1),             MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT),
 
 [CUBI_BASE] = LAYOUT_ansi_68(
-     LT(META, QK_GRAVE_ESCAPE),  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,                     KC_MINS,                 KC_EQL,  KC_BSPC,          KC_DEL,
-     LT(CUBI_MOUSE, KC_TAB),     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,                     KC_LBRC,                 KC_RBRC, KC_BSLS,          KC_HOME,
-     MO(CUBI_SYMBOL),            KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     LT(CUBI_ARROW, KC_SCLN),  LT(CUBI_ARROW, KC_QUOT),          KC_ENT,           KC_PGUP,
-     LSFT_T(KC_ESC),             KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                                                    KC_RSFT, KC_UP,   KC_PGDN,
-     KC_LCTL,                    KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,                  MO(MAC_FN1),             MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT),
+     QK_GRAVE_ESCAPE,        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,                     KC_MINS,                 KC_EQL,  KC_BSPC,          KC_DEL,
+     LT(CUBI_MOUSE, KC_TAB), KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,                     KC_LBRC,                 KC_RBRC, KC_BSLS,          KC_HOME,
+     MO(CUBI_SYMBOL),        KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     LT(CUBI_ARROW, KC_SCLN),  LT(CUBI_ARROW, KC_QUOT),          KC_ENT,           KC_PGUP,
+     LSFT_T(KC_ESC),         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                                                    KC_RSFT, KC_UP,   KC_PGDN,
+     KC_LCTL,                KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,                  MO(MAC_FN1),             MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT),
 
 [MAC_FN1] = LAYOUT_ansi_68(
      KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,            RGB_TOG,
@@ -145,11 +145,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [FN2] = LAYOUT_ansi_68(
-     KC_TILD,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_APP,   KC_RALT,  KC_RCTL,                      KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+     KC_TILD,  KC_F1,          KC_F2,         KC_F3,         KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,            KC_TRNS,
+     KC_TRNS,  TO(CUBI_BASE),  TO(MAC_BASE),  TO(WIN_BASE),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  DM_REC1,        DM_RSTP,       DM_PLY1,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,  DM_REC2,        DM_RSTP,       DM_PLY2,       KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_APP,   KC_RALT,  KC_RCTL,                      KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,        KC_TRNS,                                          KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [META] = LAYOUT_ansi_68(
      KC_TRNS,  TO(CUBI_BASE),  TO(MAC_BASE),  TO(WIN_BASE),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
--- keyboards/keychron/k7_pro/ansi/rgb/rules.mk
+++ keyboards/keychron/k7_pro/ansi/rgb/rules.mk
@@ -1,3 +1,4 @@
 # This file intentionally blank
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = custom
+DYNAMIC_MACRO_ENABLE = yes
